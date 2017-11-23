ac2bae3de90bbf8e73245d7a274f27d13a839e91
Andy Best <andybest.net@gmail.com>
2017-11-23 18:00:31 +0000

    Made pixel init public

--- Sources/SwiftImage/SwiftImage.swift
+++ Sources/SwiftImage/SwiftImage.swift
@@ -28,6 +28,12 @@ public struct PixelData {
     public var r: UInt8
     public var g: UInt8
     public var b: UInt8
+    
+    init(r: UInt8, g: UInt8, b: UInt8) {
+        self.r = r
+        self.g = g
+        self.b = b
+    }
 }
 
 public enum ImageFormat {
